3089dd36c389a913c4102f49f552167951918c74
yuckyew <yusukechayama@gmail.com>
2018-06-08 00:47:28 +0900

    Add dusheet stuff

--- install-scripts/Linux/noushi_agnoster.sh
+++ install-scripts/Linux/noushi_agnoster.sh
@@ -2,6 +2,9 @@ apt install git-core zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sudo apt install fonts-powerline
 sed -i 's/robbyrussel/agnoster/g' ~/.zshrc
-source ~/.zhsrc
+source ~/.zshrc
 chsh -s $(which zsh)
 echo "Re-login to update shell to Zsh"
+
+# Add my alias
+echo "alias hosei='sudo openconnect vpn.hosei.ac.jp'" >> ~/.zshrc
