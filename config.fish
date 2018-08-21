51e10d6e4ecaa4d26eb6a1837145c1c1d2028a4f
Yusuke Chayama <yusuke.chayama@o-11321-mac.local>
2018-08-21 17:27:40 +0900

    how to cope with pyenv

--- config.fish
+++ config.fish
@@ -1,5 +1,3 @@
-status --is-interactive; and source (pyenv virtualenv-init -|psub)
-
 ## For Bob the Fish Theme
 set -g theme_display_virtualenv yes
 
@@ -8,3 +6,4 @@ set -x GOPATH $HOME/Scrapyard/Projects/go/
 
 set -x PATH $HOME/.pyenv/bin $PATH
 . (pyenv init - | psub)
+status --is-interactive; and source (pyenv virtualenv-init -|psub)
