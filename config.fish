39dbefd27160fc79f835834e32fcacf30fb6e27a
yuckYew <yusukechayama@gmail.com>
2019-02-26 00:29:56 +0900

    update rbenv/nodebrew settings on config.fish

--- config.fish
+++ config.fish
@@ -2,8 +2,15 @@
 set -g theme_display_virtualenv yes
 
 ## For GOPATH
-set -x GOPATH $HOME/Scrapyard/Projects/go/
+set -x GOPATH $HOME/go/
 
-set -x PATH $HOME/.pyenv/bin $PATH
-. (pyenv init - | psub)
+## For pyenv
+status --is-interactive; and source (pyenv init -|psub)
 status --is-interactive; and source (pyenv virtualenv-init -|psub)
+
+## For rbenv
+set -x PATH $HOME/.rbenv/bin $PATH
+. (rbenv init - | psub)
+
+## for nodebrew
+set -x PATH $HOME/.nodebrew/current/bin $PATH
