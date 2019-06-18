dc0e72bef7557a27eed659b3fe29df5faa5629fa
Yusuke Chayama <chayay@YusukenoMacBook-Pro.local>
2019-06-19 02:04:38 +0900

    Add alot of envs :zany_face:

--- config.fish
+++ config.fish
@@ -1,11 +1,27 @@
 ## For Bob the Fish Theme
 set -g theme_display_virtualenv yes
 
+## For xterm-256color
+set -x TERM xterm-256color
+
 ## For GOPATH
-set -x GOPATH $HOME/Scrapyard/Projects/go/
+set -x GOPATH $HOME/Code/go/ $GOPATH
 
+## for goenv
+status --is-interactive; and goenv init - | source
+
+## For JAVA_HOME
+set -x JAVA_HOME /Library/Java/Home
+
+## For Pyenv
 set -x PATH $HOME/.pyenv/bin $PATH
-. (pyenv init - | psub)
-status --is-interactive; and source (pyenv virtualenv-init -|psub)
+status --is-interactive; and pyenv init - | source
+status --is-interactive; and pyenv virtualenv-init - | source
 
+## For nodebrew
+set -x PATH $HOME/.nodebrew/current/bin $PATH
+
+## Alias
 alias rp="realpath"
+alias nvim="nvim"
+alias nvim="vi"
